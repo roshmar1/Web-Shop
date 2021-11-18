1ce3ebab77d6b631d2d54424e55865fd0876a247
Martin Roshko <martin.roshko1@gmail.com>
2021-11-18 16:25:21 +0200

    new1

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
+const path = require('path');
+
 module.exports = {
     mode: 'development',
-    devServer:{
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
         static: './dist',
     },
-    devtool: "source-map"
 };
-
